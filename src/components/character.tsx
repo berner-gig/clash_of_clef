f8c597283af4da454940aead3e56d37ca0379e3a
berner <berner.gig@gmail.com>
2024-12-16 17:33:14 +0800

    feat: basic function frames

---
+++ src/components/character.tsx
@@ -0,0 +1,14 @@
+import "./character.css"
+
+function CharacterCard({id}: { id: string }) {
+
+	return (
+		<>
+			<div className="character-card">
+				<div className=""></div>
+			</div>
+		</>
+	)
+}
+
+export default CharacterCard;
---
+++ src/pages/tasks/index.tsx
@@ -0,0 +1,14 @@
+import "./task.css";
+
+function Tasks() {
+
+	return (
+		<>
+			<div className="container">
+
+			</div>
+		</>
+	)
+}
+
+export default Tasks;
