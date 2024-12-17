bb02773e126cf4b40328b18d77f5a860476b782c
berner <berner.gig@gmail.com>
2024-12-17 09:31:47 +0800

    feat: bg & icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,15 @@ import {Container, Sprite, Stage, Text} from "@pixi/react";
 import {useMemo} from "react";
 import {BlurFilter} from "pixi.js";
 
+import background from '../assests/background.png';
+
 const index = () => {
 	const blurFilter = useMemo(() => new BlurFilter(4), []);
 
 	return (
 		<>
-			<Stage width={1000} height={600} options={{background: 0xffffff}}>
-				<Sprite
-					image="https://pixijs.io/pixi-react/img/bunny.png"
-					x={400}
-					y={270}
-					anchor={{x: 0.5, y: 0.5}}
-				/>
+			<Stage width={1200} height={600} options={{background: 0xffffff}}>
+				<Sprite width={1200} height={600} image={background}/>
 
 				<Container x={400} y={330}>
 					<Text text="Hello World" anchor={{x: 0.5, y: 0.5}} filters={[blurFilter]}/>
