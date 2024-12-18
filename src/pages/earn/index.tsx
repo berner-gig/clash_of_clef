3356cffa51ff31ffc3babcf6e74b9119c6096dfc
berner <berner.gig@gmail.com>
2024-12-18 09:33:14 +0800

    feat: task page

---
+++ src/pages/earn/index.tsx
@@ -0,0 +1,133 @@
+import "./earn.css";
+
+import background from "../../assests/earn_bg.png"
+import {UserOutlined, XOutlined, DiscordOutlined} from '@ant-design/icons';
+import {Avatar, Button, Flex, List, Segmented, Typography} from "antd";
+import {ReactNode, useState} from "react";
+
+const {Paragraph} = Typography;
+
+interface Task {
+	id: number;
+	type: string
+	icon: ReactNode
+	title: string
+	description: string
+	reward: string
+}
+
+const tasks = [
+	{
+		id: 1,
+		type: 'twitter',
+		icon: <XOutlined/>,
+		title: 'Follow @Account',
+		reward: '50 Points',
+		description: 'Follow our Twitter account for latest updates'
+	},
+	{
+		id: 2,
+		type: 'telegram',
+		icon: <DiscordOutlined/>,
+		title: 'Join Community',
+		reward: '100 Points',
+		description: 'Join our Telegram community channel'
+	},
+	{
+		id: 3,
+		type: 'twitter',
+		icon: <XOutlined/>,
+		title: 'Retweet',
+		reward: '75 Points',
+		description: 'Retweet our latest announcement'
+	},
+	{
+		id: 4,
+		type: 'telegram',
+		icon: <DiscordOutlined/>,
+		title: 'Share Sticker',
+		reward: '25 Points',
+		description: 'Share our sticker pack in any group'
+	}
+]
+
+function Earn() {
+	const [activePanel, setActivePanel] = useState<string>('tasks');
+
+	return (
+		<>
+			<div className="earn-container" style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}>
+				<div className={'earn-box'}>
+
+					<div className="earn-header">
+						<Flex justify={'center'}>
+							<Avatar size={64} icon={<UserOutlined/>}/>
+						</Flex>
+						<Flex justify={'center'}>
+							<p>username</p>
+						</Flex>
+					</div>
+					<div className={'earn-content'}>
+						<Segmented
+							value={activePanel}
+							block={true}
+							onChange={(value) => setActivePanel(value)}
+							options={[
+								{
+									label: (
+										<p>Tasks</p>
+									),
+									value: 'tasks',
+								},
+								{
+									label: (
+										<p>Achievement</p>
+									),
+									value: 'achievement',
+								},
+							]}
+						/>
+						<div>
+							<div className={'earn-tasks'}>
+								<List
+									size="small"
+								>
+									{
+										tasks.map((task: Task) => (
+											<List.Item
+												key={task.id}
+												className={'task-item'}
+												extra={[
+													<Button type={'primary'}>Go</Button>
+												]}
+											>
+												<Flex
+													key={task.id}
+													gap={'small'}
+												>
+													<div>
+														<Avatar size={22} icon={task.icon}></Avatar>
+													</div>
+													<div style={{minWidth: '80%'}}>
+														<Paragraph className={'task-title'}>
+															{task.title}
+														</Paragraph>
+														<Paragraph>
+															{task.reward}
+														</Paragraph>
+													</div>
+												</Flex>
+											</List.Item>
+										))
+									}
+								</List>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Earn;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import {Container, Sprite, Stage, Text} from "@pixi/react";
 import {useMemo} from "react";
 import {BlurFilter} from "pixi.js";
 
-import background from '../assests/background.png';
+// import background from '../assests/background.png';
 
 const index = () => {
 	const blurFilter = useMemo(() => new BlurFilter(4), []);
@@ -10,11 +10,9 @@ const index = () => {
 	return (
 		<>
 			<Stage width={1200} height={600} options={{background: 0xffffff}}>
-				<Sprite width={1200} height={600} image={background}/>
+				{/*<Sprite width={1200} height={600} image={background}/>*/}
 
-				<Container x={400} y={330}>
-					<Text text="Hello World" anchor={{x: 0.5, y: 0.5}} filters={[blurFilter]}/>
-				</Container>
+
 			</Stage>
 		</>
 	)
